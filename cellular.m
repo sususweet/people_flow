c8eb25cc6ff90e2184b3a2e037cc94a0bdc660ab
sususweet <tyq9702@sina.com>
2020-01-01 14:00:51 +0800

    增加环境熟悉度影响分析

--- cellular.m
+++ cellular.m
@@ -1,4 +1,4 @@
-% 一个遵守靠右行驶的两车道的元胞自动机交通流模型
+% 一个考虑在能见度受限下行人跟随行为特性的行人流元胞自动机
 clc;
 clear;
 close all;
@@ -6,27 +6,78 @@ conf = config();
 
 space_w = 25; % 空间的宽度
 space_h = 25; % 空间的长度
-Ks = 0.4;   % 熟悉环境的行人比例
+time_step_arr = [];
 
-h = NaN;  % 图像的句柄
-per_show_time = 0.1;    %单次展示时间
+for Ks = 0:0.05:1
+    Ks = 0.4;   % 熟悉环境的行人比例
 
-iterations=400;    % 迭代次数
-prospace_wc=0.3;          % 人员密度
+    h = NaN;  % 图像的句柄
+    per_show_time = 0.1;    %单次展示时间
 
-%生成疏散空间
-[plaza,v,follow,location_pre] = create_plaza(space_w,space_h);
-% h = show_plaza(plaza,h,per_show_time);
-[plaza,v,vmax]=new_cars(plaza,v,prospace_wc);
-[plaza,v] = dist_people(plaza,v,Ks);
-% PLAZA=rot90(plaza,2);
-h=show_plaza(plaza,h,per_show_time);
+    iterations=400;    % 迭代次数
+    prospace_wc=0.3;          % 人员密度
 
-for t=1:iterations
-    [plaza,v,follow,location_pre] = move_forward(plaza,v,follow,t,location_pre);
+    %生成疏散空间
+    [plaza,v,follow,location_pre] = create_plaza(space_w,space_h);
+    % h = show_plaza(plaza,h,per_show_time);
+    [plaza,v,vmax]=new_cars(plaza,v,prospace_wc);
+    [plaza,v] = dist_people(plaza,v,Ks);
+    % PLAZA=rot90(plaza,2);
     h=show_plaza(plaza,h,per_show_time);
-%     index = find(plaza == conf.TYPE_PEOPLE_UNFAMILIAR_4);
-%     [index_i, index_j]=ind2sub(size(plaza),index);
-%     people_num = size(index,1);
-%     % disp(people_num);
+
+    time_arr=[];
+    people_num_u1=[];
+    people_num_u2=[];
+    people_num_u3=[];
+    people_num_u4=[];
+    people_num_f=[];
+    for t=1:iterations
+        [plaza,v,follow,location_pre] = move_forward(plaza,v,follow,t,location_pre);
+        h=show_plaza(plaza,h,per_show_time);
+
+        time_arr = [time_arr, t];
+        people_total = 0;
+
+        index = find(plaza == conf.TYPE_PEOPLE_UNFAMILIAR_1);
+        people_num_u1 = [people_num_u1, size(index,1)];
+        people_total = people_total + size(index,1);
+        index = find(plaza == conf.TYPE_PEOPLE_UNFAMILIAR_2);
+        people_num_u2 = [people_num_u2, size(index,1)];
+        people_total = people_total + size(index,1);
+        index = find(plaza == conf.TYPE_PEOPLE_UNFAMILIAR_3);
+        people_num_u3 = [people_num_u3, size(index,1)];
+        people_total = people_total + size(index,1);
+        index = find(plaza == conf.TYPE_PEOPLE_UNFAMILIAR_4);
+        people_num_u4 = [people_num_u4, size(index,1)];
+        people_total = people_total + size(index,1);
+        index = find(plaza == conf.TYPE_PEOPLE_FAMILIAR);
+        people_num_f = [people_num_f, size(index,1)];
+        people_total = people_total + size(index,1);
+
+        if people_total <= 0
+            break;
+        end
+    %     [index_i, index_j]=ind2sub(size(plaza),index);
+    %     people_num = size(index,1);
+    %     % disp(people_num);
+    end
+    time_step_arr = [time_step_arr, t];
 end
+figure;
+plot(0:0.05:1,time_step_arr,'LineWidth',2);
+hold on;
+xlabel('环境熟悉度'),ylabel('疏散时间步');
+
+
+% plot(time_arr,people_num_f,'Color',[183 70 255]./255,'LineWidth',2);
+% hold on;
+% plot(time_arr,people_num_u1,'Color',[255 0 0]./255,'LineWidth',2);
+% hold on;
+% plot(time_arr,people_num_u2,'Color',[255 105 41]./255,'LineWidth',2);
+% hold on;
+% plot(time_arr,people_num_u3,'Color',[19 159 255]./255,'LineWidth',2);
+% hold on;
+% plot(time_arr,people_num_u4,'Color',[0 0 255]./255,'LineWidth',2);
+% hold on;
+% xlabel('时间步'),ylabel('人数');
+% title('采用不同策略人员人数的变化');
