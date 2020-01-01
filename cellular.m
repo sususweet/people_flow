a7e26155129efdc2cf175929505c9a58062fed3c
sususweet <tyq9702@sina.com>
2020-01-01 16:01:16 +0800

    增加算例结果分析

--- cellular.m
+++ cellular.m
@@ -9,9 +9,10 @@ space_w = 25; % 空间的宽度
 space_h = 25; % 空间的长度
 time_step_arr = [];
 
-for Ks = 0:0.05:1
+% for prospace_wc = 0.1:0.1:1
     Ks = 0.4;   % 熟悉环境的行人比例
-    sight_r = 3.0/conf.cell_size;  % 行人的视野半径
+    sight_t = 3.0;
+    sight_r = sight_t/conf.cell_size;  % 行人的视野半径
 
     h = NaN;  % 图像的句柄
     per_show_time = 0.1;    %单次展示时间
@@ -59,18 +60,33 @@ for Ks = 0:0.05:1
         if people_total <= 0
             break;
         end
+        if mod(t,20) == 0
+            pause;
+        end
     %     [index_i, index_j]=ind2sub(size(plaza),index);
     %     people_num = size(index,1);
     %     % disp(people_num);
     end
     time_step_arr = [time_step_arr, t];
-end
-figure;
-plot(0:0.05:1,time_step_arr,'LineWidth',2);
-hold on;
-xlabel('环境熟悉度'),ylabel('疏散时间步');
+% end
 
 
+% figure;
+% plot(0:0.05:1,time_step_arr,'LineWidth',2);
+% hold on;
+% xlabel('环境熟悉度'),ylabel('疏散时间步');
+
+% figure;
+% plot(1:0.5:5,time_step_arr,'LineWidth',2);
+% hold on;
+% xlabel('视野半径/m'),ylabel('疏散时间步');
+
+% figure;
+% plot(0.1:0.1:1,time_step_arr,'LineWidth',2);
+% hold on;
+% xlabel('人员密度'),ylabel('疏散时间步');
+
+% figure;
 % plot(time_arr,people_num_f,'Color',[183 70 255]./255,'LineWidth',2);
 % hold on;
 % plot(time_arr,people_num_u1,'Color',[255 0 0]./255,'LineWidth',2);
